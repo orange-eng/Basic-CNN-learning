85ffd40c5fcea5c5e9265472693f85ec6d722e66
orange <972353371@qq.com>
2020-12-30 09:19:48 +0800

    change path of json

--- MobileNetV2/train.py
+++ MobileNetV2/train.py
@@ -36,7 +36,7 @@ flower_list = train_dataset.class_to_idx
 cla_dict = dict((val, key) for key, val in flower_list.items())
 # write dict into json file
 json_str = json.dumps(cla_dict, indent=4)
-with open('class_indices.json', 'w') as json_file:
+with open(path+'\\class_indices.json', 'w') as json_file:
     json_file.write(json_str)
 
 batch_size = 16
